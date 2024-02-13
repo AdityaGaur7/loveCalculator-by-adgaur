36c2786ac6c7e52d3135fdac460f425001a43187
AdityaGaur7 <adgaur027@gmail.com>
2024-02-13 23:41:23 +0530

    ok

--- public/script.js
+++ public/script.js
@@ -2,10 +2,9 @@
 let sad = ['gif/s1.gif', 'gif/s2.gif', 'gif/s3.gif', 'gif/s4.gif', 'gif/s5.gif', 'gif/s6.gif'];
 
 let cute = ['gif/l1.gif', 'gif/l4.gif', 'gif/l7.gif', 'gif/l8.gif'
-    , 'gif/l9.gif', 'gif/l14.gif', 'gif/l15.gif', 'gif/l16.gif'];
+    , 'gif/l9.gif', 'gif/l14.gif', 'gif/l15.gif', 'gif/l16.gif', 'gif/l12.gif', 'gif/l13.gif', 'gif/l17.gif', 'gif/l10.gif', 'gif/l11.gif'];
 
-let cutie = ['gif/l2.gif', 'gif/l3.gif', 'gif/l5.gif', 'gif/l6.gif', 'gif/l10.gif', 'gif/l11.gif', 'gif/l12.gif', 'gif/l13.gif', 'gif/l17.gif'
-    , 'gif/l18.gif'];
+let cutie = ['gif/l2.gif', 'gif/l3.gif', 'gif/l5.gif', 'gif/l6.gif','gif/l18.gif'];
 
     let happysong =['song/dandelion.mp3','song/timropratiksha.mp3','song/youreyes.mp3','song/alagasman.mp3','song/hosanna.mp3','song/omahi.mp3','song/wannabeyours.mp3'];
     let sadsong=['song/moyemoye.mp3','song/hasibngye.mp3','song/tuhaikahan.mp3','song/mix.mp3','song/gul.mp3','song/pahelebhimain.mp3'];
@@ -48,7 +47,7 @@ let f = () => {
         document.getElementById("gif").src = sad[random1];
         document.getElementById("text-h1").innerHTML = "Sometimes life takes unexpected turns, but trust that everything happens for a reason. 🫤 ";
 
-    } else if (random < 70) {
+    } else if (random < 80) {
         let random2 = Math.floor(Math.random() * cute.length);
         document.getElementById("gif").src = cute[random2];
         document.getElementById("text-h1").innerHTML = "in a world of billions, finding someone who complements you perfectly is a rare blessing. 💞";
