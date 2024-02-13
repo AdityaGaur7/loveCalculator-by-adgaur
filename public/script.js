700c3d98a07231809f4c9400f6229b04f673a69d
AdityaGaur7 <adgaur027@gmail.com>
2024-02-13 23:33:08 +0530

    ok

--- public/script.js
+++ public/script.js
@@ -7,8 +7,8 @@ let cute = ['gif/l1.gif', 'gif/l4.gif', 'gif/l7.gif', 'gif/l8.gif'
 let cutie = ['gif/l2.gif', 'gif/l3.gif', 'gif/l5.gif', 'gif/l6.gif', 'gif/l10.gif', 'gif/l11.gif', 'gif/l12.gif', 'gif/l13.gif', 'gif/l17.gif'
     , 'gif/l18.gif'];
 
-    let happysong =['song/dandelion.mp3','song/timropratiksha.mp3','song/youreyes.mp3'];
-    let sadsong=['song/moyemoye.mp3','song/hasibngye.mp3','song/tuhaikahan.mp3','song/mix.mp3',];
+    let happysong =['song/dandelion.mp3','song/timropratiksha.mp3','song/youreyes.mp3','song/alagasman.mp3','song/hosanna.mp3','song/omahi.mp3','song/wannabeyours.mp3'];
+    let sadsong=['song/moyemoye.mp3','song/hasibngye.mp3','song/tuhaikahan.mp3','song/mix.mp3','song/gul.mp3','song/pahelebhimain.mp3'];
 
     
 
@@ -23,7 +23,7 @@ let func1 = () => {
     let happygana = happysong[happynum];
     var audio =  new Audio(happygana);
     await audio.play();
-    audio.volume = 0.55;
+    audio.volume = 0.45;
     audio.loop = true;
    }
   async function playsadsong(){
@@ -31,7 +31,7 @@ let func1 = () => {
     let sadgana = sadsong[sadnum];
     var audio = new Audio(sadgana);
     await audio.play();
-    audio.volume = 0.55;
+    audio.volume = 0.45;
     audio.loop = true;
   
    }
@@ -42,13 +42,13 @@ let f = () => {
     let random;
     random = Math.floor(Math.random() * 100);
   
-    if (random < 40) {
+    if (random < 50) {
 
         let random1 = Math.floor(Math.random() * sad.length);
         document.getElementById("gif").src = sad[random1];
         document.getElementById("text-h1").innerHTML = "Sometimes life takes unexpected turns, but trust that everything happens for a reason. 🫤 ";
 
-    } else if (random < 60) {
+    } else if (random < 70) {
         let random2 = Math.floor(Math.random() * cute.length);
         document.getElementById("gif").src = cute[random2];
         document.getElementById("text-h1").innerHTML = "in a world of billions, finding someone who complements you perfectly is a rare blessing. 💞";
